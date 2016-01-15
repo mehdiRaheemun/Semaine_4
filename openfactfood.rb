97c95de4a7531434163a9c493c2924f9c7b6fad4
mehdi_raheemun <mraheemun@live.fr>
2016-01-15 22:04:45 +0000

    exercice ruby

---
+++ openfactfood.rb
@@ -0,0 +1,15 @@
+require 'open-url'
+require 'json'
+
+loop do
+    puts "Entrez un numero de code barre"
+    bar_code = gets.chomp
+    
+    url = "http://fr.openfoodfacts.org/api/v0/produit/#{bar_code}.json"
+    
+    json = open(url).read
+    
+    data = JSON.parse(json)
+    
+    puts date["product"]["product_name"]
+end
